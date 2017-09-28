3e6728c95edf7174b97089d9c8173d9339111813
Yusuf Hasan Miyan <yusuf.hasanmiyan@gmail.com>
2017-09-28 13:53:59 +1000

    Added sass workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,39 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var cordova = require("cordova-lib").cordova;
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('cordova-build', function(callback) {
+
   cordova.build({
     "platforms": ["browser"]
   }, callback);
+
 });
+
+gulp.task('sass', function() {
+
+  return gulp.src('./www/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./www/css'));
+
+});
+
+gulp.task('build', ['sass', 'cordova-build']);
 
 gulp.task('serve', function() {
 
   browserSync.init({
-    server: "platforms/browser/www/"
+    server: "platforms/browser/www/",
+    open: false
   });
 
-  gulp.watch("www/*.html", ['cordova-build']);
-  gulp.watch("platforms/browser/www/*.html").on('change', browserSync.reload);
+  gulp.watch(['www/sass/*.scss', 'www/sass/**/*.scss'], ['sass']);
+  gulp.watch(['www/*.html', 'www/js/*.js', 'www/css/*.css'], ['cordova-build']);
+  gulp.watch(['platforms/browser/www/*.html']).on('change', browserSync.reload);
 
 });
 
